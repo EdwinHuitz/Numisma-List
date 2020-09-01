15c17e36d45d912825abf7816ed072c90611b7fd
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-08-31 22:50:22 -0400

    adding model and formatting view files

--- models/coin-list.js
+++ models/coin-list.js
@@ -1,1 +1,15 @@
 const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const coinSchema=new Schema({
+    Coin_name:{type:String,default:'Unknown'},
+    Coin_denomination:{type:String,default:'Unknown'},
+    Coin_country:{type:String,default:'Unknown'},
+    Coin_year:{type:String,default:'Unknown'},
+    Coin_mint:{type:String,default:'No Mint-Mark'},
+    Coin_strike:{type:String,default:'Business'},
+    Coin_error:{type:String,default:'None'},
+    Coin_composition:{type:Array,default:'Unknown'},
+    Coin_grade:{type:String,default:'Ungraded'}
+})
+module.exports=mongoose.model('Coins', coinSchema);
