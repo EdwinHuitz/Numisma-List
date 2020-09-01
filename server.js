b1bd557544b6a49542bc4cd09b9e3bcd7ee715e6
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-08-31 23:45:56 -0400

    fixing error messages

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -42,9 +43,20 @@ app.use(passport.session());
 app.use('/', usersRouter);
 app.use('/auth', authRouter)
 
-// invalid request, send 404 page
-app.use(function(req, res) {
-  res.status(404).send('Cant find that!');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
