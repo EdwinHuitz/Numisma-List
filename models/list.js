dfac0ebcabd9cd279eceda877ca900e557c30f7e
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-08-31 23:27:24 -0400

    Formatting Schemas

---
+++ models/list.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+const requestSchema = new Schema({
+  User_ID:String,
+  Coins:[coinSchema]
+});
+const listSchema=new Schema({
+    Author_ID:{type:String},
+    List_title:{type:String,default:'My Coins'},
+    Coins:{type:Array,default:['']},
+    Requests:[requestSchema]
+})
+module.exports = mongoose.model('Lists', listSchema);
